f5abb3ed25169567fbd81ca4258a2c1a9f810605
Furkan Yıldırım <yildirim.frkn@outlook.com>
2025-01-22 01:36:25 +0300

    Search function

--- cypress/e2e/3-amazon/day01.cy.js
+++ cypress/e2e/3-amazon/day01.cy.js
@@ -2,19 +2,29 @@ describe("Amazon sayfasina giris", ()=>{
 
     it("Tarayiciyi başlat ve Amazon sayfasina git",()=>{
     
-        cy.visit("https://www.amazon.com.tr/")
+        //Amazon adresine git
+        cy.visit("https://www.amazon.com.tr/");
         
         cy.wait(2000);
     })
     
     it('Çerezleri kabul et', () => {
+        
+        //Çerezleri kabul et
         cy.xpath("//input[@id='sp-cc-accept']").click();
     });
     
     it('Arama kutusuna bir ürün yaz', () => {
         
-        cy.get('.ac-input-container').type('iPhone 16');
+        //Arama kutusuna iPhone 16 yaz
+        cy.get('.ac-input-container').type('iPhone 16{enter}');
+
+        //Çerezleri kabul et
+        cy.xpath("//input[@id='sp-cc-accept']").click();
         
+    });
+
+    it('', () => {
         
     });
     
